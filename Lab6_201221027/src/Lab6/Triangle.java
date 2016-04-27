fd9fc6fb4a4df6e110d06865b8990283056ef4d8
Dong Pyo <zzergling12@naver.com>
2016-04-27 16:07:52 +0900

    oo

---
+++ Lab6_201221027/src/Lab6/Triangle.java
@@ -0,0 +1,36 @@
+package Lab6;
+
+public class Triangle {
+
+	double side1;
+	double side2;
+	double side3;
+	
+	public Triangle(double side1, double side2, double side3){
+		
+		
+	}
+	
+	public double getSide1(){
+		return side1;
+	}
+	public void SetSide1(double side1){
+		
+	}
+	public double getSide2(){
+		return side2;
+	}
+	
+	public void SetSide2(double side2){
+		
+	}
+	
+	public double getSide3(){
+		return side3;
+	}
+	
+	public void SetSide3(double side3){
+		
+	}
+	
+}
