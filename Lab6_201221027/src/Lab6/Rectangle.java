ba507704a8c4440e2c07bd034219c0bdb660756e
hyemi <hyemi0320@ajou.ac.kr>
2016-04-27 16:20:18 +0900

    dd

---
+++ Lab6_201221027/src/Lab6/Rectangle.java
@@ -0,0 +1,30 @@
+package Lab6;
+
+public class Rectangle extends Shape {
+
+	double width;
+	double height;
+	
+	public Rectangle (double input_width, double input_height){
+		this. width = input_width;
+		this. height = input_height;
+		
+	}
+	
+	public double CalculateArea() {
+		return (this. width * this. height);
+	}
+	
+	public double CalculatePerimeter() {
+		return ((this. width + this. height)*2);
+		
+	}
+	
+	public void drawRectangle() {
+		
+	}
+	
+	public void printinfo() { 
+	
+	}
+}
