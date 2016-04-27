880191ce57a1db00de51e5ee55b6cd57c6ab8dfe
hyemi <hyemi0320@ajou.ac.kr>
2016-04-27 17:20:18 +0900

    dd

--- Lab6_201221027/src/Lab6/ShapeTest.java
+++ Lab6_201221027/src/Lab6/ShapeTest.java
@@ -1,18 +1,23 @@
 package Lab6;
 
 public class ShapeTest {
-
-	public void start() {
+	Shape [] shapes = new Shape[3];
+	public ShapeTest() {
 		
-		Shape [] shapes = new Shape[3];
+		
 		shapes [0] = new Rectangle(1,2);
 		shapes [1] = new Triangle(1,2,3);
 		shapes [2] = new Circle(1);
-		
+		shapes [0].setId(1);
+		shapes [1].setId(2);
+		shapes [2].setId(3);
 		
 	}
 	public static void main(String[] args) {
-		
+		ShapeTest test = new ShapeTest();
+		test.processShape(test.shapes[0]);
+		test.processShape(test.shapes[1]);
+		test.processShape(test.shapes[2]);
 		
 	}
 	
